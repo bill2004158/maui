8a4227fa1e69586574054a32b43c0940e2560874
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-03-17 23:06:38 +0100

    Updated tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18131.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18131.cs
@@ -5,7 +5,7 @@ using UITest.Core;
 namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 [Category(UITestCategories.Shape)]
-    public class Issue18131 : _IssuesUITest
+public class Issue18131 : _IssuesUITest
 {
 	public override string Issue => "Color changes are not reflected in the Rectangle shapes";
 
@@ -16,63 +16,105 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 	[Test]
 	public void CheckBackgroundColorUpdatesShapeBackgroundColor()
 	{
+		Exception? exception = null;
+
 		App.WaitForElement("ToggleBackgroundColor");
-		VerifyScreenshot("InitialShapesBackgroundandFillColors");
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "InitialShapesBackgroundandFillColors");
 
 		App.Click("ToggleBackgroundColor");
-		VerifyScreenshot();
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "");
+
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 
 	[Test]
 	public void UncheckBackgroundColorUpdatesShapeBackgroundColor()
 	{
+		Exception? exception = null;
+
 		App.WaitForElement("ToggleBackgroundColor");
-		VerifyScreenshot("InitialShapesBackgroundandFillColors");
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Initial");
 
 		App.Click("ToggleBackgroundColor");
 		App.Click("ToggleBackgroundColor");
-		VerifyScreenshot();
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_BackgroundColor");
+
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 
 	[Test]
 	public void CheckBackgroundUpdatesShapeBackground()
 	{
+		Exception? exception = null;
+
 		App.WaitForElement("ToggleBackground");
-		VerifyScreenshot("InitialShapesBackgroundandFillColors");
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Initial");
 
 		App.Click("ToggleBackground");
-		VerifyScreenshot();
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Background");
+
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 
 	[Test]
 	public void UncheckBackgroundUpdatesShapeBackground()
 	{
+		Exception? exception = null;
+
 		App.WaitForElement("ToggleBackground");
-		VerifyScreenshot("InitialShapesBackgroundandFillColors");
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Initial");
 
 		App.Click("ToggleBackground");
 		App.Click("ToggleBackground");
-		VerifyScreenshot();
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Background");
+
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 
 	[Test]
 	public void CheckFillUpdatesShapeFill()
 	{
+		Exception? exception = null;
+
 		App.WaitForElement("ToggleFill");
-		VerifyScreenshot("InitialShapesBackgroundandFillColors");
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Initial");
 
 		App.Click("ToggleFill");
-		VerifyScreenshot();
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Fill");
+
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 
 	[Test]
 	public void UncheckFillUpdatesShapeFill()
 	{
+		Exception? exception = null;
+
 		App.WaitForElement("ToggleFill");
-		VerifyScreenshot("InitialShapesBackgroundandFillColors");
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Initial");
 
 		App.Click("ToggleFill");
 		App.Click("ToggleFill");
-		VerifyScreenshot();
+		VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Fill");
+
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 }
